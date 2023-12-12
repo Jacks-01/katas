16082641bd2d782ce4264fda14aced41260b4715
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-12 16:25:50 -0600

    add function working and test for returning one number is working

--- src/kata2.js
+++ src/kata2.js
@@ -4,16 +4,28 @@ class StringCalculator {
         
         if (numberString.length > 0) {
             
-            if (numberString.includes(',')) {
-                return 'More than one number'
-            }
+            const numbersArray = this.parseString(numberString);
 
-            return parseInt(numberString);
+            const totalSum = numbersArray.reduce((sum, number) => {
+                return sum + number;
+            }, 0)
+           
+            return totalSum;
         }
 
         return 0; // Guard Clause
         
-	}
+    }
+    
+    parseString(numberString) {
+        const stringArray = numberString.split(','); // split the string into an array to add each value
+
+        let numbersArray = stringArray.map(element => {
+            return parseInt(element);
+        });
+        
+        return numbersArray;
+    }
 }
 
 module.exports = StringCalculator;
