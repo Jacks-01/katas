0d8168f65d34131f36bdbdba709f10482c2cea52
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-12 13:51:36 -0600

    added todo tests

---
+++ tests/kata1.test.js
@@ -0,0 +1,9 @@
+describe('Kata 1:  FizzBuzz', () => {
+	it.todo('returns the input as a string');
+
+	it.todo('returns Fizz if the input is a multiple of 3');
+
+	it.todo('returns Buzz if the input is a multiple of 5');
+
+	it.todo('returns FizzBuzz if the input is a multiple of 3 AND 5');
+});
