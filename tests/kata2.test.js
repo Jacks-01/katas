909cea245f0bb5031971a4124f1939833b5e6fec
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-12 16:29:18 -0600

    added todo tests to meet the requirement of the katas that I will finish later

--- tests/kata2.test.js
+++ tests/kata2.test.js
@@ -5,19 +5,22 @@ describe('Kata 2: String Calculator', () => {
 		const input = '';
 		const calculator = new StringCalculator();
 		expect(calculator.add(input)).toBe(0);
-    });
-	
+	});
+
 	it('returns the value of the number in the string if the string only has one number', () => {
-		const input = '10'
+		const input = '10';
 		const calculator = new StringCalculator();
 		expect(calculator.add(input)).toBe(10);
-    });
+	});
 	it('returns the sum of all of the numbers in the string', () => {
-		const input = '5, 5, 5'
+		const input = '5, 5, 5';
 		const calculator = new StringCalculator();
 		expect(calculator.add(input)).toBe(15);
-    });
-	
-    
+	});
 
+	it.todo('add method can parse new lines instead of commas');
+
+    it.todo('validate the string to not have the separator at the end');
+    
+	it.todo('can handle more than one delimiter');
 });
