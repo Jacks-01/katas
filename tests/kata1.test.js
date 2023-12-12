6adcdb98844d183d36df08f316ab5ab8b03488f8
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2023-12-12 14:07:03 -0600

    multiples of 5 test passing

--- tests/kata1.test.js
+++ tests/kata1.test.js
@@ -1,13 +1,16 @@
 const FizzBuzz = require('../src/kata1.js');
 
 describe('Kata 1:  FizzBuzz', () => {
-
+    
 	it('returns Fizz if the input is a multiple of 3', () => {
 		const input = 3;
 		expect(FizzBuzz(input)).toBe('Fizz');
+    });
+    
+	it('returns Buzz if the input is a multiple of 5', () => {
+		const input = 5;
+		expect(FizzBuzz(input)).toBe('Buzz');
 	});
-
-	it.todo('returns Buzz if the input is a multiple of 5');
 
 	it.todo('returns FizzBuzz if the input is a multiple of 3 AND 5');
 });
